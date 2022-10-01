7f6acb327e32770d056b413e431f1e9eab4bae71
Вероника Акулова <fenek1901@gmail.com>
2022-10-01 23:13:41 +0300

    Привет Nika

---
+++ Example006_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ведите имя пользователя:");
+string username = Console.ReadLine ();
+if (username.ToLower () == "nika")
+{
+    Console.WriteLine("Ура, это же NIKA!");
+}
+else
+{
+Console.Write("Привет,");
+Console.WriteLine(username);
+}

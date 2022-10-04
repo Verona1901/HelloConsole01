45dc086aa9d54aecd98014bb77daef555b3568e3
Вероника Акулова <fenek1901@gmail.com>
2022-10-05 01:32:35 +0300

    Example010_MetodArray

---
+++ Example010_MetodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {1,21,3,45,5,21,7,45};
+int n = array.Length;
+int find =21;
+int index =0;
+while (index<n)
+{
+    if (array[index] ==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
